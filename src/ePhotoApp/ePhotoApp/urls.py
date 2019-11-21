6af502bb3160ed1e97fac348926cc33281d73081
Ubuntu <ubuntu@ip-172-31-20-193.us-east-2.compute.internal>
2019-11-21 07:35:59 +0000

    deploy

--- src/ePhotoApp/ePhotoApp/urls.py
+++ src/ePhotoApp/ePhotoApp/urls.py
@@ -26,8 +26,8 @@ urlpatterns = [
 
 # 以下を定義
 from django.conf import settings
-from django.conf.urls.static import static
-
-
+#from django.conf.urls.static import static
+from django.conf.urls.static import  static
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
